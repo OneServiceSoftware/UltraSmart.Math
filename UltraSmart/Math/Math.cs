ba7c334f93b1ca56a8b7f4f6b68f5ba676edd4d7
Alexander <malveiralexander@outlook.com>
2018-01-10 22:15:55 -0300

    Added: factorial, summation with and without repeating, and module

--- UltraSmart/Math/Math.cs
+++ UltraSmart/Math/Math.cs
@@ -6,6 +6,41 @@ namespace UltraSmart.Math
 {
     public static class Math
     {
+        public static double Combinatorial(double numN,double numR)
+        {
+            double result;
+            result = Facorials(numN) / Facorials(numR) * Facorials(numN - numR);
+            return result;            
+        }
 
+        public static double CombinatorialR(double numN, double numR)
+        {
+            double result;
+            result = Facorials(numN + numR - 1) / Facorials(numR) * Facorials(numN - 1);
+            return result;
+        }
+
+        public static double Sum(double num)
+        {
+            double result;
+            result = num * (num + 1) / 2;
+            return result;
+        }
+
+        public static double Mod(double num1, double num2)
+        {
+            return num1 % num2;
+        }
+
+        public static double Facorials(double num)
+        {
+            double factor;
+            factor = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factor = factor * i;
+            }
+            return factor;
+        }
     }
 }
